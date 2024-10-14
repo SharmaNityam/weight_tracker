c2409da9578419eaefbfc1f0e285879be07b701e
Nityam Sharma <142774674+SharmaNityam@users.noreply.github.com>
2024-10-14 23:29:16 +0530

    Update user_settings.dart

--- lib/models/user_settings.dart
+++ lib/models/user_settings.dart
@@ -3,16 +3,19 @@ import 'package:flutter/material.dart';
 class UserSettings {
   final String userName;
   final TimeOfDay notificationTime;
+  final ThemeMode themeMode;
 
   UserSettings({
     this.userName = '',
     this.notificationTime = const TimeOfDay(hour: 9, minute: 0),
+    this.themeMode = ThemeMode.dark, // Set default to dark
   });
 
   Map<String, dynamic> toMap() {
     return {
       'userName': userName,
       'notificationTime': '${notificationTime.hour}:${notificationTime.minute}',
+      'themeMode': themeMode.index,
     };
   }
 
@@ -24,6 +27,19 @@ class UserSettings {
         hour: int.parse(timeParts[0]),
         minute: int.parse(timeParts[1]),
       ),
+      themeMode: ThemeMode.values[map['themeMode'] ?? ThemeMode.dark.index],
+    );
+  }
+
+  UserSettings copyWith({
+    String? userName,
+    TimeOfDay? notificationTime,
+    ThemeMode? themeMode,
+  }) {
+    return UserSettings(
+      userName: userName ?? this.userName,
+      notificationTime: notificationTime ?? this.notificationTime,
+      themeMode: themeMode ?? this.themeMode,
     );
   }
 }
